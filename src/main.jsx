a3bab043b3d767b8d44f5916e0857d8c9d8051e1
Robotsrule4545 <caleb.mincraft18@gmail.com>
2025-03-13 13:13:02 +0800

    Test tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>You've reached the right place :)</div>
+    <div class="text-lg font-semibold text-black">You've reached the right place :)</div>
   </StrictMode>,
 )
